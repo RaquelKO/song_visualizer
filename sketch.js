e52fc9ccad56e3f72bb3b5f23c3c2a62d8d01719
RaquelKO <kuntzraquel@gmail.com>
2021-03-04 18:48:13 -0300

    change colors

--- sketch.js
+++ sketch.js
@@ -8,14 +8,14 @@ function preload() {
 function setup() {
   createCanvas(displayWidth, displayHeight);
   amplitude = new p5.Amplitude();
-  // console.log("click the pink circle to start!");
+  // console.log("click the yellow circle to start!");
 }
 
 function draw() {
-  background('black');
+  background(2);
   let level = amplitude.getLevel();
   let diameter = map(level, 0, 1, 10, 400);
-  fill(231,84,128);
+  fill('yellow');
   noStroke();
   ellipse(displayWidth/2,displayHeight/2,diameter,diameter);
 }
